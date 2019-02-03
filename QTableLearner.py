607879db6134d5885cf29c3bc0ec01880b7fa832
Subhajit Das <jeetudas95@gmail.com>
2019-02-04 00:26:12 +0530

    More functions added to QTableLearner

--- QTableLearner.py
+++ QTableLearner.py
@@ -4,6 +4,9 @@ import platform
 from time import sleep, time
 import numpy as np
 
+RENDER_REFRESH_TIME = 0.02
+NON_RENDER_REFRESH_TIME = 0.008
+
 
 class QTableLearner:
 
@@ -24,12 +27,22 @@ class QTableLearner:
         self.avg_time = 0
         self.steps_per_episode = 100
 
-    def _render_logs(self, episode, total_episodes, epsilon, step, action, reward, done, done_count):
+    def _clear_screen(self):
+        """Clears the terminal screen according to OS.
+
+        Parameters: None
+
+        Returns: None
+        """
+        os.system('cls') if platform.system() == \
+            'Windows' else os.system('clear')
+
+    def _render_train_logs(self, episode, total_episodes, epsilon, step, action, reward, done, done_count):
         """Rendering text logs on console window.
 
         Parameters:
         episode (int): Running episode number.
-        train_episodes (int): The total number of gameplay episodes to learn from for agent.
+        total_episodes (int): The total number of gameplay episodes.
         epsilon (int): Exploration Exploitation tradeoff rate for running episode.
         step (int): Step count for current episode.
         action (int): Action taken for the current state of environment.
@@ -41,8 +54,7 @@ class QTableLearner:
         """
 
         # Clear Screen
-        os.system('cls') if platform.system() == \
-            'Windows' else os.system('clear')
+        self._clear_screen()
 
         # Printing Logs
         print(f'Model Name     :\t{self.model_name}')
@@ -55,14 +67,14 @@ class QTableLearner:
         print(f'Episode Done ? :\t{"Yes" if done else "No"}')
         print(f'Done Count     :\t{done_count}')
 
-    def _render_env(self):
+    def _render_train_env(self):
         """Renders the environment."""
         print()
         self.env.render()
 
-    def _render_time(self, episode_left, episode_t,  step_t, done, step_end, render, wait=0.02):
+    def _render_train_time(self, episode_left, episode_t,  step_t, done, step_end, render):
         """ Calculates and renders time metrics for training.
-        
+
         Parameters:
         episode_left (int): Number of episodes left out of total episodes.
         episode_t (int): Running episode time in seconds.
@@ -87,8 +99,7 @@ class QTableLearner:
         print(f'Average Episode Time :\t{np.round(self.avg_time,4)} secs')
         print(f'Current Episode Time :\t{np.round(episode_t,4)} secs')
         print(f'Current Step Time    :\t{np.round(step_t,4)} secs')
-        if render:
-            sleep(wait)
+        sleep(RENDER_REFRESH_TIME if render else NON_RENDER_REFRESH_TIME)
 
     def train(self, train_episodes=10000, lr=0.7, gamma=0.6, render=False):
         """ Calling this method will start the training process.
@@ -124,11 +135,11 @@ class QTableLearner:
                 # Keeping track of done count
                 done_count += 1 if episode_done else 0
                 # Rendering Logs
-                self._render_logs(episode, train_episodes, epsilon, curr_step, curr_action,
-                                  reward, episode_done, done_count)
+                self._render_train_logs(episode, train_episodes, epsilon, curr_step, curr_action,
+                                        reward, episode_done, done_count)
                 # Rendering environment
                 if render:
-                    self._render_env()
+                    self._render_train_env()
                 # Updating QTable using Bellman Equation
                 self.qtable[curr_state, curr_action] = \
                     self.qtable[curr_state, curr_action] + lr*(reward + gamma * max(self.qtable[new_state, :]) -
@@ -138,8 +149,8 @@ class QTableLearner:
 
                 # Step Time Calculation
                 t_step = time() - t_s_step
-                self._render_time(train_episodes-episode, t_episode,
-                                  t_step, episode_done, self.steps_per_episode-1 == curr_step, render)
+                self._render_train_time(train_episodes-episode, t_episode,
+                                        t_step, episode_done, self.steps_per_episode-1 == curr_step, render)
 
                 if episode_done:
                     break
@@ -152,6 +163,48 @@ class QTableLearner:
             t_episode = time()-t_s_episode
         self.env.close()
 
+    def _render_test_logs(self, episode, total_episodes, step, episode_reward, step_reward, done, done_count):
+        """Rendering text logs on console window.
+
+        Parameters:
+        episode (int): Running episode number.
+        total_episodes (int): The total number of gameplay episodes.
+        step (int): Step count for current episode.
+        episode_reward (int): Reward for the action taken in the environment for current episode.
+        step_reward (int): Reward for the action taken in the environment for current step.
+        done (bool): Flag to know where the episode is finished or not.
+        done_count (int): Counter for how many time the agent finished the episode before timeout.
+
+        Returns: None
+        """
+
+        # Clear Screen
+        self._clear_screen()
+
+        # Printing Logs
+        print(f'Model Name     :\t{self.model_name}')
+        print(f'Episode Number :\t{episode}/{total_episodes}')
+        print(f'Episode Step   :\t{step+1}/100')
+        print(f'Episode Reward :\t{episode_reward}')
+        print(f'Step Reward    :\t{step_reward}')
+        print(f'Episode Done ? :\t{"Yes" if done else "No"}')
+        print(f'Done Count     :\t{done_count}')
+
+    def _render_test_env(self, render):
+        """Renders the environment
+
+        Parameters:
+        render (bool): Flag to render the environment or not.
+
+        Returns: None
+        """
+
+        if render:
+            self.env.render()
+            sleep(RENDER_REFRESH_TIME)
+        else:
+            sleep(NON_RENDER_REFRESH_TIME)
+
     def test(self, test_episodes=200, render=False):
         """ Testing method to know our environment performance.
 
@@ -162,31 +215,84 @@ class QTableLearner:
         Returns: None
         """
         self.env.reset()
+        # Collecting the rewards over time.
         rewards = list()
         for episode in range(test_episodes):
             state = self.env.reset()
-            step = 0
-            done = False
-            total_rewards = 0
-            for step in range(self.steps_per_episode):
-                if render:
-                    # Clear Screen
-                    os.system('cls') if platform.system() == \
-                        'Windows' else os.system('clear')
-                    self.env.render()
-                    sleep(0.02)
+            # Reward for current episode.
+            total_rewards, done_count = 0, 0
+            for _ in range(self.steps_per_episode):
+                # Selecting the best action.
                 action = np.argmax(self.qtable[state, :])
-
+                # Performing the action.
                 new_state, reward, done, info = self.env.step(action)
 
                 total_rewards += reward
-                print ("Score", total_rewards)
+                # Printing logs
+                self._render_test_logs(
+                    episode, test_episodes, _, total_rewards, reward, done, done_count)
+                # Render Environment
+                self._render_test_env(render)
+
                 if done:
                     rewards.append(total_rewards)
+                    done_count += 1
                     break
+                # Changing states for next step
                 state = new_state
         self.env.close()
-        print("Score over time: " + str(sum(rewards)/test_episodes))
+        print(f"\n\nScore over time: \t{sum(rewards)/test_episodes}")
+
+    def set_refresh_time(self, time, render):
+        """ Sets the refresh time for render mode TRUE or FALSE.
+
+        Parameters:
+        time (float): Refresh time in seconds.
+        render (bool): Render flag for which time parameter is to be set.
+
+        Returns: None
+        """
+        if render:
+            RENDER_REFRESH_TIME = time
+        else:
+            NON_RENDER_REFRESH_TIME = time
+
+    def save_model(self, model_name=None, model_path='saved_models'):
+        """ Save the QTable in storage for future use.
+
+        Parameters:
+        model_name (str): Takes the model name to save the file.
+                          If None is given it will take the default model_name (default: None)
+        model_path (str): Folder path of the location where model should be saved.
+
+        Returns: None
+        """
+
+        if not model_name:
+            model_name = self.model_name
+
+        if not os.path.isdir(model_path):
+            os.makedirs(model_path)
+
+        path = os.path.join(model_path, model_name)
+        np.save(path, self.qtable)
+        print(f'Model saved at location :\t{path}')
+        sleep(3)
+
+    def load_model(self, model_name):
+        """ Load the QTable from storage.
+
+        Parameters:
+        model_name (str): The path of the model with extension .npy
+
+        Returns: None
+        """
+        if not os.path.isfile(model_name):
+            print(f'File not found for the location {model_name}')
+        else:
+            self.qtable = np.load(model_name)
+            print(f'Model loaded from location {model_name}')
+        sleep(3)
 
 
 if __name__ == "__main__":
@@ -200,5 +306,7 @@ if __name__ == "__main__":
     #model = 'NChain-v0'
     #model = 'Roulette-v0'
     taxi_driver = QTableLearner(gym.make(model), model)
-    taxi_driver.train(train_episodes=100,render=False)
-    taxi_driver.test(test_episodes=10,render=True)
+    taxi_driver.load_model(os.path.join('saved_models', model+'.npy'))
+    taxi_driver.train(train_episodes=10, render=False)
+    taxi_driver.save_model()
+    taxi_driver.test(test_episodes=10, render=True)
